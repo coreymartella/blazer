35e3109af00bf377af0df09b810d5b835a185730
Corey Martella <cmartella@miovision.com>
2017-04-26 14:22:50 -0400

    fluid containers and audit fix

--- app/models/blazer/audit.rb
+++ app/models/blazer/audit.rb
@@ -1,6 +1,9 @@
 module Blazer
   class Audit < ActiveRecord::Base
-    belongs_to :user, Blazer::BELONGS_TO_OPTIONAL.merge(class_name: Blazer.user_class.to_s)
+    if Blazer.user_class.present? && Blazer.user_class.constantize < ActiveRecord::Base
+      belongs_to :user, Blazer::BELONGS_TO_OPTIONAL.merge(class_name: Blazer.user_class.to_s)
+    end
+    
     belongs_to :query, Blazer::BELONGS_TO_OPTIONAL
   end
 end
